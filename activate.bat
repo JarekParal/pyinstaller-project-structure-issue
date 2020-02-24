4c80cfbcc956171be9112f0cab626ec701418d40
Jaroslav Páral <jparal@lambda-x.com>
2020-02-24 11:39:10 +0100

    Commit build output for two different python files (`calmanimo.py` and `test.py`) but with exactly same content

--- activate.bat
+++
@@ -1,1 +0,0 @@
-call conda activate test
--- build-test.bat
+++ build-test.bat
@@ -1,4 +1,4 @@
-pyinstaller calmanimo.py ^
+pyinstaller test.py ^
     --add-data "settings.json;." --add-data "tests_data/10D.NIMOW;tests_data/." ^
     --add-data "calmanimo/GUI/icons/*;calmanimo/GUI/icons/" ^
     --add-binary "calmanimo/devices/clementor/dll/*;." --version-file "CI/version.py" ^
